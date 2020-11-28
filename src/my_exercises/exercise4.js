69fb4e850cdc64a96fb8d3a87bb277dce3296caf
geva <gevahitman@gmail.com>
2020-11-28 13:28:09 +0200

    add ex 4

---
+++ src/my_exercises/exercise4.js
@@ -0,0 +1,8 @@
+const { fromHttpRequest } = require('../utils/http');
+const {mergeAll, count} = require('rxjs/operators');
+
+
+fromHttpRequest('https://orels-moviedb.herokuapp.com/movies').pipe(
+    mergeAll(),
+    count()
+).subscribe(console.log)
